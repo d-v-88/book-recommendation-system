0cd4aa75395e3907666939ed942c8505a4217516
d-v-88 <priyavarma808@gmail.com>
2026-03-15 10:02:52 +0530

    Setup for the project added.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+## edit below variables as per your requirements -
+REPO_NAME = "ML Based Books Recommender System"
+AUTHOR_USER_NAME = "DHITI NAVRATAN VARMA"
+SRC_REPO = "books_recommender"
+LIST_OF_REQUIREMENTS = []
+
+
+setup(
+    name=SRC_REPO,
+    version="0.0.1",
+    author="DHITI NAVRATAN VARMA",
+    description="A small local packages for ML based books recommendations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/entbappy/ML-Based-Book-Recommender-System",
+    author_email="dhitivarma2004gmail.com",
+    packages=find_packages(),
+    license="MIT",
+    python_requires=">=3.7",
+    install_requires=LIST_OF_REQUIREMENTS
+)
